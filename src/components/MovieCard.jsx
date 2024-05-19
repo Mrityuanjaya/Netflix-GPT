e65029cf83cbaab779b3cbaa79f2a17c024d2e2d
Mrityuanjaya <mrityuanjaya.gupta@joshtechnologygroup.com>
2024-05-19 12:33:54 +0530

    BugFix: Included key property while using map in Movie List

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,6 @@
 import { IMG_CDN_URL } from "../utils/constants"
 
 const MovieCard = ({title, posterPath}) => {
-    console.log(IMG_CDN_URL+posterPath);
   return (
     <div className="w-48 pr-4">
         <img src={IMG_CDN_URL + posterPath } alt={title + " Poster"}/>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ const MovieList = ({ title, movies }) => {
       <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex mb-4">
           {movies?.map((movie) => (
-            <MovieCard title={movie?.title} posterPath={movie?.poster_path} />
+            <MovieCard key={movie?.id} title={movie?.title} posterPath={movie?.poster_path} />
           ))}
         </div>
       </div>
